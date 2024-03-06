214c94b3b3e793efade416b26dfc1b56999e67a8
asma jawaid <arshadasma45678@gmail.com>
2024-03-06 12:13:50 +0500

    new commit

---
+++ step1_helloWorld/main.ts
@@ -0,0 +1,6 @@
+//Assignment NO 1
+// storing a person name in variables
+
+let personName : string = "Asma,"
+ //Printing a message to the person
+ console.log(`Hello ${personName} would you like to learn some Coding today?`)
---
+++ step2_NameCases/main.ts
@@ -0,0 +1,17 @@
+//storing a person name in a veriable
+
+let personName : string = "Asma jawaid"
+
+// printing a person name in lowercase
+
+console.log("Lowercase:", personName.toLowerCase() )
+
+// printing a person name in Uppercase
+
+console.log("uppercase:", personName.toUpperCase() )
+
+// printing a person name in titlecase 
+
+
+console.log("Titlecase:", personName.toTitlecase() )
+
